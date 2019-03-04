1efe4d7f1b70691a1260884e3587538e2b76aec3
Prince Odame <opodame@gmail.com>
2019-03-04 00:53:47 +0000

    Restructure project for separation of concerns

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // main entry point of the application
-const inquirer = require('inquirer');
-const columnify = require('columnify')
+const { inquirer, getMatrixDimensionsFromUser, getMatrixElementsFromUser } = require('./cli');
+const columnify = require('columnify');
 
 /** Print a nicely formatted version of a matrix to console */
 const printMatrix = (matrix, title) => {
@@ -9,9 +9,7 @@ const printMatrix = (matrix, title) => {
 
     const colOptions = {};
     for (let col = 0; col < numCols; col++)
-        colOptions[`${col}`] = {
-            headingTransform: () => ' ' // we dont want any headings for the columns
-        }
+        colOptions[`${col}`] = { headingTransform: () => ' ' } // use space as headings
 
     const matrixString = columnify(
         matrix,
@@ -19,13 +17,13 @@ const printMatrix = (matrix, title) => {
             columnSplitter: '  ', // add a little more space between elements in the same row
             config: colOptions
         }
-    );
+    ).trim(); // trim() to remove the 'space-as-heading's
 
-    console.log('\n******************************');
-    console.log(`${title}  (${numRows}X${numCols})`);
-    console.log('******************************');
+    console.log('***************************');
+    console.log(`${title} | (${numRows}X${numCols})`);
+    console.log('***************************');
     console.log(matrixString);
-    console.log('******************************\n');
+    console.log('***************************');
 }
 
 /**
@@ -43,110 +41,32 @@ const getTranspose = (matrix) => {
         const tRow = [];
         // a column becomes a row in the transposed matrix
         for (let row = 0; row < numRows; row++)
-            tRow.push(matrix[row][col]) // notice how we swap col and row
+            tRow.push(matrix[row][col])
         tMatrix.push(tRow);
     }
 
     return tMatrix
 }
 
-const isPositiveWholeNumber = (number_) => {
-    const asFloat = Number.parseFloat(number_);
-    const asInt = Number.parseInt(number_);
 
-    return asInt == asFloat && asInt > 0;
-}
 
-const positiveNumberInputValidator = (input) => {
-    return new Promise((resolve) => {
-        resolve(
-            isPositiveWholeNumber(input) ?
-                true : 'You must enter a POSITIVE INTEGER'
-        )
-    })
-};
+const run = async () => {
+    console.log('Welcome.\nThis programme is used to determine the transpose of a matrix.');
 
-const rowInputValidator = (numCols) => {
-    return (input) => {
-        return new Promise((resolve) => {
-            // squash multiple spaces into one
-            const elements = input.toString().trim().split(/[ ]+/);
-            resolve(
-                elements.length === numCols ?
-                    true : `You must enter exactly ${numCols} items separated by spaces`
-            );
-        })
-    }
-}
+    console.log('To start, enter the dimensions of your matrix');
+    const dimensions = await getMatrixDimensionsFromUser(inquirer);
 
-/**
- * Join multiple validators for inquirer prompt inputs
- * @param  {...()=>Promise<boolean>} validators See https://github.com/SBoudrias/Inquirer.js#objects
- * for more information on the structure of validators
- */
-const joinValidators = (...validators) => {
-    return new Promise(resolve => {
-        Promise.all(validators.map(v => v()))
-            .then(validatorValues => {
-                for (const value of validatorValues)
-                    if (value !== true)
-                        resolve(value) // resolve the first validator that fails
-                resolve(true); // resolve true if no validator failed
-            })
-    })
-}
+    console.log('Enter the elements of each row as a space-separated list');
+    const inputMatrix = await getMatrixElementsFromUser(
+        dimensions.numRows, dimensions.numCols, inquirer
+    );
+    printMatrix(inputMatrix, 'Original Matrix');
 
-/** Read the user's input from stdin and parse it into a matrix */
-const promptUserForInput = () => new Promise((resolve) => {
-    const dimensionsQuestions = [
-        {
-            type: 'number',
-            name: 'numRows',
-            message: "NUMBER OF ROWS >>",
-            validate: positiveNumberInputValidator
-        },
-        {
-            type: 'number',
-            name: 'numCols',
-            message: "NUMBER OF COLUMNS >>",
-            validate: positiveNumberInputValidator
-        }
-    ];
-
-    console.log('Welcome.\nThis programme is used to determine the transpose of a matrix.')
-    console.log('To start, please enter the dimensions of your matrix');
-    inquirer.prompt(dimensionsQuestions).then((answers) => {
-        const numRows = Number.parseInt(answers.numRows)
-        const numCols = Number.parseInt(answers.numCols);
-
-        const rowsQuestions = [];
-        for (let i = 0; i < numRows; i++)
-            rowsQuestions.push({
-                type: 'input',
-                name: `row_${i}`,
-                message: `Row ${i}>>`,
-                validate: rowInputValidator(numCols),
-            })
-
-        console.log('Please enter the elements of each row as a space-separated list');
-        inquirer.prompt(rowsQuestions).then((rowsAnswers) => {
-            const matrix = [];
-            for (let row = 0; row < numRows; row++)
-                matrix.push(rowsAnswers[`row_${row}`].trim().split(/[ ]+/))
-            resolve(matrix);
-        })
-
-    })
-});
-
-promptUserForInput().then((matrix) => {
-    printMatrix(matrix, 'Original Matrix');
-    const transposedMatrix = getTranspose(matrix);
+    const transposedMatrix = getTranspose(inputMatrix);
     printMatrix(transposedMatrix, 'Transposed Matrix');
-})
-    .catch((err) => {
-        console.error('An error occurred while running script');
-        console.error(err);
-    })
+}
 
-
+run().catch(error => {
+    console.error('An error occurred while running script');
+    console.error(error);
+});
---
+++ cli.js
@@ -0,0 +1,108 @@
+const inquirer = require('inquirer');
+
+const isPositiveWholeNumber = (number_) => {
+    const asFloat = Number.parseFloat(number_);
+    const asInt = Number.parseInt(number_);
+
+    return asInt == asFloat && asInt > 0;
+}
+
+const positiveNumberInputValidator = (input) => {
+    return new Promise((resolve) => {
+        resolve(
+            isPositiveWholeNumber(input) ?
+                true : 'You must enter a POSITIVE INTEGER'
+        )
+    })
+};
+
+const makeRowInputValidator = (numCols) => {
+    return (input) => {
+        return new Promise((resolve) => {
+            // squash multiple spaces into one
+            const elements = input.toString().trim().split(/[ ]+/);
+            resolve(
+                elements.length === numCols ?
+                    true : `You must enter exactly ${numCols} items separated by spaces`
+            );
+        })
+    }
+}
+
+/**
+ * Join multiple validators for inquirer prompt inputs
+ * @param  {...()=>Promise<boolean>} validators See https://github.com/SBoudrias/Inquirer.js#objects
+ * for more information on the structure of validators
+ */
+const joinValidators = (...validators) => {
+    return new Promise(resolve => {
+        Promise.all(validators.map(v => v()))
+            .then(validatorValues => {
+                for (const value of validatorValues)
+                    if (value !== true)
+                        resolve(value) // resolve the first validator that fails
+                resolve(true); // resolve true if no validator failed
+            })
+    })
+}
+
+/**
+ * Get number of rows and number of columns from user input (stdin)
+ * @param {inquirer} inquirer
+ * @returns {Promise<{numRows: number, numCols: number}>}
+ */
+const getMatrixDimensionsFromUser = (inquirer) => new Promise((resolve, reject) => {
+    const dimensionsQuestions = [
+        {
+            type: 'number',
+            name: 'numRows',
+            default: 3,
+            message: "NUMBER OF ROWS >>",
+            validate: positiveNumberInputValidator
+        },
+        {
+            type: 'number',
+            name: 'numCols',
+            default: 3,
+            message: "NUMBER OF COLUMNS >>",
+            validate: positiveNumberInputValidator
+        }
+    ];
+    inquirer.prompt(dimensionsQuestions).then((answers) => resolve({
+        'numRows': Number.parseInt(answers.numRows),
+        'numCols': Number.parseInt(answers.numCols)
+    })).catch(error => reject(error))
+});
+
+/**
+ * Get the elements of a matrix from user input (stdin)
+ * @param {inquirer} inquirer
+ * @param {number} numRows Expected number of rows of the matrix
+ * @param {number} numCols Expected number of columns of the matrix
+ * @returns {Promise<number[][]>}
+ */
+const getMatrixElementsFromUser = (numRows, numCols, inquirer) => new Promise((resolve, reject) => {
+    const rowsQuestions = [];
+    // we will prompt user to enter the elements row by row
+    for (let i = 0; i < numRows; i++)
+        rowsQuestions.push({
+            type: 'input',
+            name: `row_${i}`,
+            message: `Row ${i}>>`,
+            validate: makeRowInputValidator(numCols),
+        })
+
+    inquirer.prompt(rowsQuestions).then((rowsAnswers) => {
+        const matrix = [];
+        for (let row = 0; row < numRows; row++)
+            matrix.push(rowsAnswers[`row_${row}`].trim().split(/[ ]+/))
+        resolve(matrix);
+    }).catch(error => reject(error))
+});
+
+
+module.exports = {
+    inquirer,
+    getMatrixDimensionsFromUser,
+    getMatrixElementsFromUser
+}
